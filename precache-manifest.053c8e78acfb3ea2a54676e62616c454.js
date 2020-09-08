8363017f158b438df098e1f1e55a3b7d2ec793e9
Chloe Agape <chloe@binti.com>
2020-09-07 22:22:14 -0700

    Updates

---
+++ precache-manifest.053c8e78acfb3ea2a54676e62616c454.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b8ae8a72abce5df46c2aaf0098970e",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "15d9876363531ab4c165",
+    "url": "/about/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "219e4d2d5ec667749d57",
+    "url": "/about/static/js/2.dfab6112.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/about/static/js/2.dfab6112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d9876363531ab4c165",
+    "url": "/about/static/js/main.4cbac4ac.chunk.js"
+  },
+  {
+    "revision": "a0062db1e7ece8554a7a",
+    "url": "/about/static/js/runtime-main.dacf2f0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/about/static/media/logo.5d5d9eef.svg"
+  }
+]);
